b9e9767464f9fca25afbd3465a774d0e944938fd
shoronshaha <smaranshaha@gmail.com>
2022-03-24 01:52:49 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const API_KEY = `40e019b90e8e0909da22e06077405a2e`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+}
